e405ba35355624a449beeaced5071681407e4901
¨AlfredoHernandez45¨ <l18390045@chetumal.tecnm.mx>
2021-09-10 04:30:43 -0500

    updated

--- analizador.java
+++ analizador.java
@@ -1,20 +1,18 @@
 import java.util.*;
 public class analizador {
     
-    ArrayList arreglo = new ArrayList();
-    ArrayList pila = new ArrayList();
-    
-    public void main (String [] args){
+    public static void main (String [] args){
         //declarar variables
         Scanner scan = new Scanner(System.in);
-        Object exp;
+        char list[];
+        String exp;
 
-        
         //salida y entrada de datos
         System.out.println("Ingresar expresion");
         exp = scan.nextLine();
+        list = exp.toCharArray();
         
-        System.out.println(exp);
+        AL(list);
     }
 
     /**
@@ -22,23 +20,49 @@ public class analizador {
      * analisa la expresión ingresada para indicar si es valida 
      * con el lenguaje (*,+,-,/,0...9)
      */
-    public void AL(){
+    public static void AL(char list[]){
+        //Declarar varibales
+        ArrayList<Object> lista = new ArrayList<Object>();
+        char val [] = {'0','1','2','3','4','5','6','7','8','9','+','-','*','/'};
+        
+        //Verifica que la expresion cumpla con el lenguaje (*,+,-,/,0...9)
+        for(int i=0; i<list.length; i++){
+            boolean chek = false;
 
+            //Se compara la expresion y el lenguaje
+            for(int j=0; j<val.length; j++){
+                
+                if(list[i] == val[j]){
+                    lista.add(list[i]);;
+                    chek = true;
+                }
+                if(chek == true) break;
+            }
+
+        }
+        if(list.length == lista.size()) {
+            //ASi();
+        }else {
+            System.out.println("ERROR LEXICO \nLa expresion no cumple con el Analisis Lexico, no se puede continuar");
+        };
     }
     
     /**
      * Analisis Sintactico
      * se realiza el posfijo y el arbol sintactico
      */
-    public void ASi(){
-
+    public static void ASi(){
+        //ArrayList<Object> arreglo = new ArrayList<Object>();
+        //ArrayList<Object> pila = new ArrayList<Object>();
+        //ArrayList<Object> len = new ArrayList<Object>();
+        //len.add();
     }
     
     /**
      * Analisis Semantico
      * se realiza el arbol
      */
-    public void ASe(){
+    public static void ASe(){
 
     }
 }
